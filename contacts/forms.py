fbeaf2502d2778fdfc5d1f45da5008ff4b4c5e02
yastreb <roman.makhunov@ingenios.io>
2026-04-10 14:49:45 +0300

    Integrate django-bootstrap5 for improved UI/UX: update dependencies, settings, and templates. Simplify contact form rendering using bootstrap tags.

--- contacts/forms.py
+++ contacts/forms.py
@@ -13,25 +13,21 @@ class ContactForm(forms.ModelForm):
             "name": forms.TextInput(
                 attrs={
                     "placeholder": _("Ваше имя"),
-                    "class": "form-control form-control-lg bg-light border-0",
                 }
             ),
             "phone": forms.TextInput(
                 attrs={
                     "placeholder": _("+375..."),
-                    "class": "form-control form-control-lg bg-light border-0",
                 }
             ),
             "email": forms.EmailInput(
                 attrs={
                     "placeholder": _("example@mail.com"),
-                    "class": "form-control form-control-lg bg-light border-0",
                 }
             ),
             "comment": forms.Textarea(
                 attrs={
                     "placeholder": _("Опишите вашу задачу..."),
-                    "class": "form-control form-control-lg bg-light border-0",
                     "rows": 4,
                 }
             ),
